be81139f0c5cadc7ce2c1e416d14604c45f3ac78
Pradipgyawali897 <gyawalipradip846@gmail.com>
2026-02-23 00:17:50 +0545

    New XML files are added new repo to test the parser are added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+use std::process::exit;
 fn main() {
-    println!("Hello, world!");
+    let filepath = "docs.gl/gl4/glClear.xhtml";
+    let content = fs::read_to_string(filepath).unwrap_or_else(|err| {
+        eprint!("Error : could not open the file {filepath}:{err}");
+        exit(1);
+    });
+    println!("Length of {filepath} is {length}", length = content.len());
 }
