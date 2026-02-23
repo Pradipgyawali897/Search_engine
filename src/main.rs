064b114e0de3034448e4ae73779564c4e9d4bef7
Pradipgyawali897 <gyawalipradip846@gmail.com>
2026-02-23 22:20:39 +0545

    Added a xml pardor xml parsor was made the parsor parse from gven xml file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
-use std::fs;
-use std::process::exit;
+use core::panic;
+#[warn(unused)]
+use std::fs::{self, ReadDir};
+use std::{fs::File, io};
+use xml::reader::{EventReader, XmlEvent};
+
+fn read_entire_xml_file(file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let file = File::open(file_path)?;
+    let er = EventReader::new(file);
+    let mut content = String::new();
+
+    for event in er.into_iter() {
+        if let XmlEvent::Characters(text) = event? {
+            content.push_str(&text);
+        }
+    }
+
+    Ok(content)
+}
+
 fn main() {
     let filepath = "docs.gl/gl4/glClear.xhtml";
-    let content = fs::read_to_string(filepath).unwrap_or_else(|err| {
-        eprint!("Error : could not open the file {filepath}:{err}");
-        exit(1);
+    let fir_path = "docs.gl/gl4";
+    let dir = fs::read_dir(fir_path).unwrap_or_else(|err| {
+        panic!("Error with reading the file :{err}");
     });
-    println!("Length of {filepath} is {length}", length = content.len());
+
+    for file in dir {
+        println!("{file:?}");
+    }
+    /*println!(
+        "{} is the content ",
+        read_entire_xml_file(filepath).expect("Todo")
+    );*/
 }
