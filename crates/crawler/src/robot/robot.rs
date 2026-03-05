f5dba77e6b1baa852b12cfe4362895e1055a8008
Pradipgyawali897 <gyawalipradip846@gmail.com>
2026-03-05 12:13:00 +0545

    Make robot content retrieval asynchronous   added debug logging for robots.txt checks.

--- crates/crawler/src/robot/robot.rs
+++ crates/crawler/src/robot/robot.rs
@@ -2,8 +2,8 @@ use super::server::check_robot;
 use super::utils::parse_robots;
 use super::utils::Robots;
 
-pub fn get_robot_content(domain: &str)->Option<Robots>{
-    let is_robot=check_robot(domain);
+pub async fn get_robot_content(domain: &str)->Option<Robots>{
+    let is_robot=check_robot(domain).await;
     match is_robot {
         Ok(Some(content)) => {
             let robots = parse_robots(&content);
@@ -11,7 +11,10 @@ pub fn get_robot_content(domain: &str)->Option<Robots>{
             Some(robots)
         }
         Ok(None) => None,
-        Err(e) => None,
+        Err(e) => {
+            println!("Error: {:#?}", e); 
+            None
+        },
     }
     
 }
--- crates/crawler/src/robot/server.rs
+++ crates/crawler/src/robot/server.rs
@@ -2,8 +2,10 @@ use reqwest;
 
 pub async fn check_robot(domain: &str) -> Result<Option<String>, reqwest::Error> {
     let url = format!("https://{}/robots.txt", domain);
+    println!("Checking robots.txt for {}", url);
 
     let resp = reqwest::get(&url).await?;
+    println!("Response: {:#?}", resp);
 
     if resp.status() == reqwest::StatusCode::NOT_FOUND {
         return Ok(None);
